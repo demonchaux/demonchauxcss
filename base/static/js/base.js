2c3f5a66bb9991a9c5d37a6ebddeb02834da7e87
Alexandr Godetskiy <godetskiy@gmail.com>
2014-07-10 16:37:43 +0300

    Absolute paper layout

--- base/static/js/base.js
+++ base/static/js/base.js
@@ -54,9 +54,7 @@ $(document).ready(function(){
         });
     }
     scrollHeader();
-});
 
-$(window).load(function(){
     function initGallery() {
         var gallerias = $('.galleria');
         if (gallerias.length == 0) {
@@ -64,23 +62,90 @@ $(window).load(function(){
         }
         $.each(gallerias, function(index, item){
             var id = '#' + $(item).attr('id');
-            $(id).galleria({
-                showInfo: false,
-                thumbnails: false,
-                imageCrop: true
+            $(id).bxSlider({
+                // adaptiveHeight: true,
+                // auto: true,
+                pager: false
             });
         });
-//        if ($('.galleria').length > 0) {
-//
-//        Galleria.run(".galleria", {
-//            showInfo: false,
-//            thumbnails: false,
-//            imageCrop: "width"
-//        });
-        Galleria.ready(function(){
-            $('.slide .galleria > img').remove();
-        });
-//    }
     }
     initGallery();
+
+    function paperLayout() {
+        Array.prototype.repeat= function(what, L){
+            while(L) this[--L]= what;
+            return this;
+        }
+        Array.prototype.max = function(comparer) {
+            if (this.length === 0) return null;
+            if (this.length === 1) return this[0];
+            comparer = (comparer || Math.max);
+            var v = this[0];
+            for (var i = 1; i < this.length; i++) {
+                v = comparer(this[i], v);
+            }
+            return v;
+        }
+        var blocks = $('.cblock'),
+            columns,
+            perLine,
+            indent,
+            left,
+            top,
+            maxHeight = [];
+
+        columns = 4;
+        indent = 20;
+        currCount = columns;
+        bWidth = $(blocks).first().width();
+        maxHeight.repeat(0, columns);
+
+        var matrix = [],
+            arr = [];
+
+        $.each(blocks, function(index, item){
+            arr.push($(item));
+            currCount -= 1;
+            if (currCount == 0) {
+                currCount = columns;
+                matrix.push(arr);
+                arr = [];
+            }
+        });
+        if (arr.length != 0) {
+            matrix.push(arr);
+        }
+
+        top = 0;
+        $.each(matrix, function(i, line) {
+            left = 0;
+            $.each(line, function(j, item) {
+                maxHeight[j] += item.height() + parseInt(item.css('padding-top')) + parseInt(item.css('padding-bottom'));
+                if (i != 0) {
+                    // Calculate top
+                    var el = matrix[i - 1][j],
+                        height;
+                    height = el.height() + parseInt(el.css('padding-top')) + parseInt(el.css('padding-bottom'));
+                    top = height + indent;
+                }
+                if (j != 0) {
+                    // Calculate left
+                    var el = matrix[i][j - 1],
+                        width;
+                    width = el.width() + parseInt(el.css('padding-left')) + parseInt(el.css('padding-right'));
+                    left += width + indent;
+                }
+                item.css({
+                    'position': 'absolute',
+                    'left': left,
+                    'top': top
+                });
+            });
+        });
+        console.log(maxHeight.max());
+        $('.main').css({
+            'height': maxHeight.max() + 2 * indent
+        });
+    }
+    paperLayout();
 });
